ae3054ce6c585e8fdea0a7c4800e0b8d33c07e63
Lasry BESKIWIN <lasrybeskiwin@gmail.com>
2024-06-02 13:59:31 +0200

    Ajout Class TestOperation

---
+++ Partie 2/TestOperation.java
@@ -0,0 +1,22 @@
+public class TestOperation {
+    public static void main(String[] args) {
+        try {
+            // Création des instances de Nombre
+            Expression deux = new Nombre(2);
+            Expression trois = new Nombre(3);
+            Expression dixSept = new Nombre(17);
+
+            // Création des opérations
+            Expression s = new Soustraction(dixSept, deux);
+            Expression a = new Addition(deux, trois);
+            Expression d = new Division(s, a);
+
+            // Affichage du résultat
+            System.out.println(d + " = " + d.valeur()); // doit afficher : ((17 - 2) / (2 + 3)) = 3.0
+
+        } catch (IllegalArgumentException e) {
+            // Gestion de l'exception pour la division par zéro
+            System.err.println("Erreur: " + e.getMessage());
+        }
+    }
+}
